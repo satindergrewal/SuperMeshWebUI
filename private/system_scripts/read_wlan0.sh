6e7fd26e7fd01f9a2dddf056eb0705037b7d437b
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2015-12-30 03:27:02 +1300

    network settings update
    - shell script reading network interface updated
    - javascript updated to read and manage web interface behavriour
    - Network settings Web UI updated

--- private/system_scripts/read_wlan0.sh
+++ private/system_scripts/read_wlan0.sh
@@ -13,8 +13,29 @@ file='./test/interfaces'
 #System interfes file
 #file='/etc/network/interfaces'
 
-addr=$(awk ' f && NR==f+1; /iface wlan0/ {f=NR}' $file | awk '{print $2}')
-netmask=$(awk ' f && NR==f+2; /iface wlan0/ {f=NR}' $file | awk '{print $2}')
-gateway=$(awk ' f && NR==f+3; /iface wlan0/ {f=NR}' $file | awk '{print $2}')
+eth0_dhcp_static=$(awk ' f && NR==f+1; /auto eth0/ {f=NR}' $file | awk '{print $4}')
+if [ "$eth0_dhcp_static" == "static" ]; then
+	eth0_addr=$(awk ' f && NR==f+1; /iface eth0/ {f=NR}' $file | awk '{print $2}')
+	eth0_netmask=$(awk ' f && NR==f+2; /iface eth0/ {f=NR}' $file | awk '{print $2}')
+	eth0_gateway=$(awk ' f && NR==f+3; /iface eth0/ {f=NR}' $file | awk '{print $2}')
+else
+	eth0_addr=''
+	eth0_netmask=''
+	eth0_gateway=''
+fi
 
-echo '[{"eth0":{"addr":"'$addr'","netmask":"'$netmask'","gateway":"'$gateway'"}}]'
+wlan0_dhcp_static=$(awk ' f && NR==f+2; /auto wlan0/ {f=NR}' $file | awk '{print $4}')
+if [ "$wlan0_dhcp_static" == "static" ]; then
+	wlan0_addr=$(awk ' f && NR==f+1; /iface wlan0/ {f=NR}' $file | awk '{print $2}')
+	wlan0_netmask=$(awk ' f && NR==f+2; /iface wlan0/ {f=NR}' $file | awk '{print $2}')
+	wlan0_gateway=$(awk ' f && NR==f+3; /iface wlan0/ {f=NR}' $file | awk '{print $2}')
+else
+	wlan0_addr=''
+	wlan0_netmask=''
+	wlan0_gateway=''
+fi
+
+cat << EOF
+[{"eth0":{"dhcp_static":"$eth0_dhcp_static","addr":"$eth0_addr","netmask":"$eth0_netmask","gateway":"$eth0_gateway"}},
+{"wlan0":{"dhcp_static":"$wlan0_dhcp_static","addr":"$wlan0_addr","netmask":"$wlan0_netmask","gateway":"$wlan0_gateway"}}]
+EOF
--- public/js/network.js
+++ public/js/network.js
@@ -15,6 +15,44 @@ $(document).ready(function() {
     $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 */
 
+    //Update LAN0 input values on DHCP/Static options selection change
+    $('input[type=radio][name=eth0_dhcp_satic]').change(function() {
+        if (this.value == 'dhcp') {
+            $( "#eth0_dhcp" ).prop( "checked", true );
+            $( "#eth0_ip_address" ).val('');
+            $( "#eth0_subnet_mask" ).val('');
+            $( "#eth0_gateway" ).val('');
+            $( "#eth0_ip_address" ).prop( "disabled", true );
+            $( "#eth0_subnet_mask" ).prop( "disabled", true );
+            $( "#eth0_gateway" ).prop( "disabled", true );
+        }
+        else if (this.value == 'static') {
+            $( "#eth0_static" ).prop( "checked", true );
+            $( "#eth0_ip_address" ).prop( "disabled", false );
+            $( "#eth0_subnet_mask" ).prop( "disabled", false );
+            $( "#eth0_gateway" ).prop( "disabled", false );
+        }
+    });
+    
+    //Update WLAN0 input values on DHCP/Static options selection change
+    $('input[type=radio][name=wlan0_dhcp_satic]').change(function() {
+        if (this.value == 'dhcp') {
+            $( "#wlan0_dhcp" ).prop( "checked", true );
+            $( "#wlan0_ip_address" ).val('');
+            $( "#wlan0_subnet_mask" ).val('');
+            $( "#wlan0_gateway" ).val('');
+            $( "#wlan0_ip_address" ).prop( "disabled", true );
+            $( "#wlan0_subnet_mask" ).prop( "disabled", true );
+            $( "#wlan0_gateway" ).prop( "disabled", true );
+        }
+        else if (this.value == 'static') {
+            $( "#wlan0_static" ).prop( "checked", true );
+            $( "#wlan0_ip_address" ).prop( "disabled", false );
+            $( "#wlan0_subnet_mask" ).prop( "disabled", false );
+            $( "#wlan0_gateway" ).prop( "disabled", false );
+        }
+    });
+
 });
 
 // Functions =============================================================
@@ -27,10 +65,33 @@ function populateIPInfo() {
 
     // jQuery AJAX call for JSON
     $.getJSON( '/admin/network/getip', function( data ) {
-    	//console.log(data[0].eth0);
-    	$( "#ip-address" ).val( data[0].eth0.addr );
-    	$( "#subnet-mask" ).val( data[0].eth0.netmask );
-    	$( "#gateway" ).val( data[0].eth0.gateway );
+    	//console.log(data[0].wlan0);
+
+        //Enable/Disable and feed eth0 data to form input fields
+        if ( data[0].eth0.dhcp_static == 'dhcp' ) {
+            $( "#eth0_dhcp" ).prop( "checked", true );
+            $( "#eth0_ip_address" ).prop( "disabled", true );
+            $( "#eth0_subnet_mask" ).prop( "disabled", true );
+            $( "#eth0_gateway" ).prop( "disabled", true );
+        } else {
+            $( "#eth0_static" ).prop( "checked", true );
+            $( "#eth0_ip_address" ).val( data[0].eth0.addr );
+            $( "#eth0_subnet_mask" ).val( data[0].eth0.netmask );
+            $( "#eth0_gateway" ).val( data[0].eth0.gateway );
+        }
+
+        //Enable/Disable and feed wlan0 data to form input fields
+        if ( data[1].wlan0.dhcp_static == 'dhcp' ) {
+            $( "#wlan0_dhcp" ).prop( "checked", true );
+            $( "#wlan0_ip_address" ).prop( "disabled", true );
+            $( "#wlan0_subnet_mask" ).prop( "disabled", true );
+            $( "#wlan0_gateway" ).prop( "disabled", true );
+        } else {
+            $( "#wlan0_static" ).prop( "checked", true );
+            $( "#wlan0_ip_address" ).val( data[1].wlan0.addr );
+            $( "#wlan0_subnet_mask" ).val( data[1].wlan0.netmask );
+            $( "#wlan0_gateway" ).val( data[1].wlan0.gateway );
+        }
 
 
         // For each item in our JSON, add a table row and cells to the content string
