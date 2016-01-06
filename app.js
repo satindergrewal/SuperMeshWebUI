2754b94fb8d0c477cb21b69395773301f63b5cf1
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-01-07 11:45:31 +1300

    Telepathy Messaging GUI added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var SuperMesh = require("./private/js/app_functions.js");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var network = require('./routes/network');
+var telepathy = require('./routes/telepathy');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.locals.apptitle = "SuperMesh Node";
 app.use('/', routes);
 app.use('/users', users);
 app.use('/admin/network', network);
+app.use('/admin/telepathy', telepathy);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
